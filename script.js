bbb90e86b43843701d1ae1756182c9c70de05315
Lita Beach <litabeach123@gmail.com>
2021-01-26 20:16:09 -0600

    added map to page

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 10,
-  //   this is the meat of the app -- zoom, select long/lat, styles
-  //  how to implement places in here 
+    //   this is the meat of the app -- zoom, select long/lat, styles
+    //  how to implement places in here 
   });
 }
+  
+
+
