e840a132e6b265f3918d869228d8688b92144895
Lita Beach <litabeach123@gmail.com>
2021-01-27 14:26:12 -0600

    search + map work!

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let map;
 
-//setting city to a particular value for testing
-var city = "Minneapolis"
-// geolocate URL
+function initMap(city) {
+  // geolocate URL
 geolocateURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=AIzaSyBepTaWB2S-ZswMELWF7HxBIvUDpXCAG9o"
 console.log(geolocateURL)
 
-function initMap() {
   // AJAX call for geolocate
 $.ajax({
   url: geolocateURL,
@@ -14,12 +12,9 @@ $.ajax({
 
 })
   .then(function (response) {
-    console.log(response)
-    //setting the variables for longitude and latitude to plug in to line 5 for centering
+    //setting the variables for longitude and latitude to plug in to line 20 to center
     var latOne = response.results[0].geometry.location.lat
-    console.log(latOne)
     var lonOne = response.results[0].geometry.location.lng
-    console.log(lonOne)
  
       map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: latOne, lng: lonOne },
@@ -28,14 +23,13 @@ $.ajax({
   });
 }
 
-
 // on-click event for search button
 $("#search").on("click", function (event) {
   event.preventDefault();
   //sets the variable "city" to the value of the input div
   var city = $("#enter-city").val().trim();
   if (city) {
-    initMap()
+    initMap(city)
   // here run the function that updates the map 
   // here run the function that populates the page with local attractions and restaurants
   }
