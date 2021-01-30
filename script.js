b79eb5080c6f32601a70717171254953c5e0740d
Lita Beach <litabeach123@gmail.com>
2021-01-30 14:10:25 -0600

    turned restaurants and hotels builders into functions

--- script.js
+++ script.js
@@ -36,194 +36,13 @@ function initMap(city) {
       });
 
       userLocate();
-
-      var request = {
-        location: new google.maps.LatLng(latOne, lonOne),
-        radius: 1500,
-        type: ['restaurant']
-      };
-
-
-      var service = new google.maps.places.PlacesService(map);
-      service.nearbySearch(request, function (results, status) {
-        $(".restaurant-container-md").empty();
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < 10; i++) {
-            var name = results[i].name;
-            var placeID = results[i].place_id;
-            var photo = results[i].photos[0].getUrl
-            var rating = results[i].rating;
-            var price = results[i].price_level;
-            var address = results[i].vicinity;
-            // var hours = results[i].opening_hours.isOpen
-            // console.log(hours)
-
-            //set restaurant div = to HTML rest cont div
-            var restarauntDiv = $(".restaurant-container-md")
-
-            //creating new row 
-            var createRow= $("<div class= 'row'>")
-            
-            //creating new image column
-            var newImgCol= $("<div class= 'col-md-5'>")
-
-            //creating new description column
-            var newDescriptCol= $("<div class='col-md-7'>")
-
-
-            //create a <p> for the name, call it nameEl and set the value of name to the name variable
-            var nameEl = $("<p>");
-            nameEl.html(name);
-
-            // var hoursEl = $("<p>");
-            // hoursEl.html(hours);
-            var addressEl = $("<p>");
-            addressEl.html(address);
-
-            var ratingEl = $("<p>");
-            ratingEl.html("Rating: " + rating + " stars");
-
-            //photo
-            var photoEl = $("<img class='photo-size'>");
-            photoEl.attr("src", photo);
-
-            //price
-            var priceEl = $("<p>");
-            priceEl.html(price);
-            //change to for loop?
-            // for (var i = 0; i < price.length; i++)
-            if (price == 1) {
-              priceEl = "$"
-            }
-            if (price == 2) {
-              priceEl = "$$"
-            }
-            if (price == 3) {
-              priceEl = "$$$"
-            }
-            if (price == 4) {
-              priceEl = "$$$$"
-            }
-            if (price == 5) {
-              priceEl = "$$$$$"
-            }
-
-            //line break
-            linebreak = $("<br>")
-
-            //save button
-            var restBtn = $("<button>");
-            restBtn.html("Save to List");
-
-            //add it to the page
-            restarauntDiv.append(createRow);
-            createRow.append(newImgCol);
-            createRow.append(newDescriptCol);
-            newImgCol.append(photoEl);
-            newDescriptCol.append(nameEl);
-            newDescriptCol.append(addressEl);
-            newDescriptCol.append(ratingEl);
-            newDescriptCol.append(priceEl);
-            newDescriptCol.append(linebreak);
-            newDescriptCol.append(restBtn);
-            
-
-            var marker = new google.maps.Marker({
-              place: {
-                placeId: placeID,
-                location: results[i].geometry.location
-              },
-              title: name,
-            });
-            marker.setMap(map);
-          }
-          map.setCenter(results[0].geometry.location);
-        }
-      })
-
-      var request2 = {
-        location: new google.maps.LatLng(latOne, lonOne),
-        radius: 1500,
-        type: ['lodging']
-      };
-
-      service.nearbySearch(request2, function (results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          $(".attract-container-md").empty()
-          for (var i = 0; i < 10; i++) {
-            var name = results[i].name;
-            var placeID = results[i].place_id;
-            var photo = results[i].photos[0].getUrl
-            var rating = results[i].rating;
-            var address = results[i].vicinity
-
-            // var hours = results[i].opening_hours.isOpen
-            // console.log(hours)
-
-            //set restaurant div = to HTML rest cont div
-            var hotelDiv = $(".attract-container-md")
-
-            //creating new row
-            var newRow = $("<div class= 'row'>")
-
-            //create new image column
-            var imageCol = $("<div class= 'col-md-5'>")
-
-            //create new description column
-            var descriptionCol = $("<div class='col-md-7'>")
-
-            //create a <p> for the name, call it nameEl and set the value of name to the name variable
-            var nameEl = $("<p>");
-            nameEl.html(name);
-
-            // var hoursEl = $("<p>");
-            // hoursEl.html(hours);
-
-            var ratingEl = $("<p>");
-            ratingEl.html("Rating: " + rating + " stars");
-
-            //photo
-            var photoEl = $("<img class='photo-size'>");
-            photoEl.attr("src", photo);
-
-            //address
-            var addressEl = $("<p>");
-            addressEl.html(address);
-
-            //save button
-            var hotelBtn = $("<button>");
-            hotelBtn.html("Save to List");
-
-            //add it to the page
-            hotelDiv.append(newRow);
-            newRow.append(imageCol);
-            newRow.append(descriptionCol);
-            imageCol.append(photoEl);
-            descriptionCol.append(nameEl);
-            descriptionCol.append(addressEl);
-            descriptionCol.append(ratingEl);
-            descriptionCol.append(hotelBtn);
-
-            // (restarauntDiv).append(hoursEl);
-
-            var marker = new google.maps.Marker({
-              place: {
-                placeId: placeID,
-                location: results[i].geometry.location
-              },
-              title: name,
-            });
-
-            marker.setMap(map);
-          }
-          map.setCenter(results[0].geometry.location);
-        }
-        console.log(results, status)
-      }
-      );
-    }
+      addRestaurants(latOne, lonOne);
+      addHotels(latOne, lonOne);
+      
+   }
   });
 }
+
 
 function userLocate() {
   infoWindow = new google.maps.InfoWindow();
@@ -264,6 +83,7 @@ $("#search").on("click", function (event) {
   var city = $("#enter-city").val().trim();
   if (city) {
     initMap(city);
+    addRestaurants();
   }
 });
 
@@ -278,3 +98,192 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.open(map);
 }
 
+function addHotels(latOne, lonOne) {
+  var request2 = {
+    location: new google.maps.LatLng(latOne, lonOne),
+    radius: 1500,
+    type: ['lodging']
+  };
+
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request2, function (results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      $(".attract-container-md").empty()
+      for (var i = 0; i < 10; i++) {
+        var name = results[i].name;
+        var placeID = results[i].place_id;
+        var photo = results[i].photos[0].getUrl
+        var rating = results[i].rating;
+        var address = results[i].vicinity
+
+        // var hours = results[i].opening_hours.isOpen
+        // console.log(hours)
+
+        //set restaurant div = to HTML rest cont div
+        var hotelDiv = $(".attract-container-md")
+
+        //creating new row
+        var newRow = $("<div class= 'row'>")
+
+        //create new image column
+        var imageCol = $("<div class= 'col-md-5'>")
+
+        //create new description column
+        var descriptionCol = $("<div class='col-md-7'>")
+
+        //create a <p> for the name, call it nameEl and set the value of name to the name variable
+        var nameEl = $("<p>");
+        nameEl.html(name);
+
+        // var hoursEl = $("<p>");
+        // hoursEl.html(hours);
+
+        var ratingEl = $("<p>");
+        ratingEl.html("Rating: " + rating + " stars");
+
+        //photo
+        var photoEl = $("<img class='photo-size'>");
+        photoEl.attr("src", photo);
+
+        //address
+        var addressEl = $("<p>");
+        addressEl.html(address);
+
+        //save button
+        var hotelBtn = $("<button>");
+        hotelBtn.html("Save to List");
+
+        //add it to the page
+        hotelDiv.append(newRow);
+        newRow.append(imageCol);
+        newRow.append(descriptionCol);
+        imageCol.append(photoEl);
+        descriptionCol.append(nameEl);
+        descriptionCol.append(addressEl);
+        descriptionCol.append(ratingEl);
+        descriptionCol.append(hotelBtn);
+
+        // (restarauntDiv).append(hoursEl);
+
+        var marker = new google.maps.Marker({
+          place: {
+            placeId: placeID,
+            location: results[i].geometry.location
+          },
+          title: name,
+        });
+
+        marker.setMap(map);
+      }
+      map.setCenter(results[0].geometry.location);
+    }
+    console.log(results, status)
+  }
+  );
+}
+
+function addRestaurants(latOne, lonOne){
+  var request = {
+    location: new google.maps.LatLng(latOne, lonOne),
+    radius: 1500,
+    type: ['restaurant']
+  };
+
+
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, function (results, status) {
+    $(".restaurant-container-md").empty();
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < 10; i++) {
+        var name = results[i].name;
+        var placeID = results[i].place_id;
+        var photo = results[i].photos[0].getUrl
+        var rating = results[i].rating;
+        var price = results[i].price_level;
+        var address = results[i].vicinity;
+        // var hours = results[i].opening_hours.isOpen
+        // console.log(hours)
+
+        //set restaurant div = to HTML rest cont div
+        var restarauntDiv = $(".restaurant-container-md")
+
+        //creating new row 
+        var createRow= $("<div class= 'row'>")
+        
+        //creating new image column
+        var newImgCol= $("<div class= 'col-md-5'>")
+
+        //creating new description column
+        var newDescriptCol= $("<div class='col-md-7'>")
+
+
+        //create a <p> for the name, call it nameEl and set the value of name to the name variable
+        var nameEl = $("<p>");
+        nameEl.html(name);
+
+        // var hoursEl = $("<p>");
+        // hoursEl.html(hours);
+        var addressEl = $("<p>");
+        addressEl.html(address);
+
+        var ratingEl = $("<p>");
+        ratingEl.html("Rating: " + rating + " stars");
+
+        //photo
+        var photoEl = $("<img class='photo-size'>");
+        photoEl.attr("src", photo);
+
+        //price
+        var priceEl = $("<p>");
+        priceEl.html(price);
+        //change to for loop?
+        // for (var i = 0; i < price.length; i++)
+        if (price == 1) {
+          priceEl = "$"
+        }
+        if (price == 2) {
+          priceEl = "$$"
+        }
+        if (price == 3) {
+          priceEl = "$$$"
+        }
+        if (price == 4) {
+          priceEl = "$$$$"
+        }
+        if (price == 5) {
+          priceEl = "$$$$$"
+        }
+
+        //line break
+        linebreak = $("<br>")
+
+        //save button
+        var restBtn = $("<button>");
+        restBtn.html("Save to List");
+
+        //add it to the page
+        restarauntDiv.append(createRow);
+        createRow.append(newImgCol);
+        createRow.append(newDescriptCol);
+        newImgCol.append(photoEl);
+        newDescriptCol.append(nameEl);
+        newDescriptCol.append(addressEl);
+        newDescriptCol.append(ratingEl);
+        newDescriptCol.append(priceEl);
+        newDescriptCol.append(linebreak);
+        newDescriptCol.append(restBtn);
+        
+
+        var marker = new google.maps.Marker({
+          place: {
+            placeId: placeID,
+            location: results[i].geometry.location
+          },
+          title: name,
+        });
+        marker.setMap(map);
+      }
+      map.setCenter(results[0].geometry.location);
+    }
+  })
+}
