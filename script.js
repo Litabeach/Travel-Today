3ad55f42bc17a7f8d353be5910dc0e8c39e8ff4f
Bao Xiong <bao.xiong2011@gmail.com>
2021-02-01 21:38:26 -0600

    Added saved item html and stylized with css. Function to make nav bar work is in javascript.

--- script.js
+++ script.js
@@ -276,3 +276,12 @@ function addRestaurants(latOne, lonOne) {
   })
 }
 
+function openNav() { 
+  document.getElementById( 
+      "myNav").style.height = "100%"; 
+} 
+
+function closeNav() { 
+  document.getElementById( 
+      "myNav").style.height = "0%"; 
+} 
